328dd879bb5144131e940fc504bbb7870c69484e
Иванов Евгений Викторович <5595gecka@gmail.com>
2017-03-01 14:57:48 +0700

    Kek

--- deposit.c
+++ deposit.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 
 int days, sum;
+
 void DataInput(){
     printf("Enter days:");
     scanf("%d", &days);
@@ -8,18 +9,32 @@ void DataInput(){
     scanf("%d", &sum);
 }
 void InputCheck(){
-  if( days < 30 || days > 360 ||
+  if( days < 30 || days > 365 ||
       sum < 1000){
     printf("Incorrect data input\n");
-    printf("\n Enter again");
+    printf("\n Enter again\n");
     DataInput();
     InputCheck();
   }
 }
-
+int Count(){
+  if(days >= 30)
+    sum = sum + (sum * 0.1);
+  if(days >= 31)
+    sum = sum + (sum * 0.02);
+  if(days >= 120)
+    sum = sum + (sum * 0.06);
+  if(days = 360)
+    sum = sum + (sum * 0.12);
+  return sum;
+}
+  
 int main(){
     DataInput();
     InputCheck();
+    Count();
 
-    return 0;
+    printf("\nDeposit sum = %d\n",sum );
+
+      return 0;
 }
