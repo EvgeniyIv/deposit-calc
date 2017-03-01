452a916290bae5dfb1b1ba606e5c4dfa38e89a60
Иванов Евгений Викторович <5595gecka@gmail.com>
2017-03-01 14:33:38 +0700

    Check

--- deposit.c
+++ deposit.c
@@ -7,9 +7,19 @@ void DataInput(){
     printf("Enter sum:");
     scanf("%d", &sum);
 }
+void InputCheck(){
+  if( days < 30 || days > 360 ||
+      sum < 1000){
+    printf("Incorrect data input\n");
+    printf("\n Enter again");
+    DataInput();
+    InputCheck();
+  }
+}
+
 int main(){
     DataInput();
-
+    InputCheck();
 
     return 0;
 }
